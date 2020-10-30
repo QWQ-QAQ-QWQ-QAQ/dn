98ff0ea51c92c8be154420e5e6b9dbe40f7d0862
qwq <7955928+qwwq-qwq-qwwq@user.noreply.gitee.com>
2020-10-31 00:05:45 +0800

    jc

---
+++ dd - 副本.py
@@ -0,0 +1,8 @@
+a="种瓜得瓜"
+print(a.find("瓜"))
+for a in range(1,10,3):
+    print(a)
+n=[1,2,3,4,5,6]
+print(n)
+n.insert(2,8)
+print(n)
---
+++ dd.py
@@ -0,0 +1,17 @@
+import turtle
+t = turtle.Turtle()
+turtle.screensize(400,300,"green")
+turtle.screensize(400,300,"red")
+turtle.screensize(400,300,"green")
+t.seth(30)
+t.forward(150)
+t.seth(10)
+t.forward(150)
+t.heading()
+t.hideturtle()
+t.setx(120)#横
+t.sety(-120)#竖
+t.circle(100,steps=5)
+t.circle(100,180)
+t.circle(100,180,steps=5)
+t.circle(100,180,steps=5)
